dde5d48cc108e3afc939548e51bb87e551b7c5f8
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-01-28 18:54:30 +0530

    Signup page

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,26 @@
+import { Button } from "../components/authComponents/Button";
+import { ButtonWarning } from "../components/authComponents/ButtonWarning";
+import { Heading } from "../components/authComponents/Heading";
+import { InputBox } from "../components/authComponents/InputBox";
+import { SubHeading } from "../components/authComponents/SubHeading";
+
+export function Signup() {
+    return <div className=" bg-slate-200 h-screen flex justify-center">
+        <div className="flex flex-col justify-center">
+            <div className="bg-gray-200 rounded-lg w-80 text-center p-2 px-4">
+                <Heading label="Sign Up" />
+                <SubHeading label="Enter the following details to create your account" />
+                <InputBox onChange={() => {
+
+                }} label="Username" placeholder="John"/>
+                <InputBox onChange={() => {
+                    
+                }} label="Password" placeholder="*******"/>
+                <Button onClick={() => {
+
+                }} label="Sign Up" />
+                <ButtonWarning label="Already have an account? " buttonText="Signin" to={"/sigin"} />
+            </div>
+        </div>
+    </div>
+}
