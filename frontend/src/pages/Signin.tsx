a47fb3edd523462dc9aa8057c2b6ae71c18654d3
Sadashivamurthy S P <sadashivamurthysp10656@gmail.com>
2025-01-29 00:55:14 +0530

    auth FE & BE integration

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import { Button } from "../components/authComponents/Button";
 import { ButtonWarning } from "../components/authComponents/ButtonWarning";
 import { Heading } from "../components/authComponents/Heading";
 import { InputBox } from "../components/authComponents/InputBox";
 import { SubHeading } from "../components/authComponents/SubHeading";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function Signin() {
+    const[userName, setUserName] = useState("");
+    const[password, setPassword] = useState("");
+    const navigate = useNavigate();
+
     return <div className=" bg-slate-200 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="bg-gray-200 rounded-lg w-80 text-center p-2 px-4">
                 <Heading label="Sign In" />
                 <SubHeading label="Enter the following details to login to your account" />
-                <InputBox onChange={() => {
-
+                <InputBox onChange={(e) => {
+                    setUserName(e.target.value);
                 }} label="Username" placeholder="John"/>
-                <InputBox onChange={() => {
-                    
+                <InputBox onChange={(e) => {
+                    setPassword(e.target.value);
                 }} label="Password" placeholder="*******"/>
-                <Button onClick={() => {
-
+                <Button onClick={async() => {
+                    const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+                        userName,
+                        password
+                    });
+                    localStorage.setItem("token", response.data.token);
+                    navigate("/dashboard");
+                    alert("You are signed in");
                 }} label="Sign In" />
                 <ButtonWarning label="Don't have an account? " buttonText="SignUp" to={"/signup"} />
             </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import { Button } from "../components/authComponents/Button";
 import { ButtonWarning } from "../components/authComponents/ButtonWarning";
 import { Heading } from "../components/authComponents/Heading";
 import { InputBox } from "../components/authComponents/InputBox";
 import { SubHeading } from "../components/authComponents/SubHeading";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function Signup() {
-    return <div className=" bg-slate-200 h-screen flex justify-center">
+    const[userName, setUserName] = useState("");
+    const[password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    return <div className="bg-slate-200 h-screen flex justify-center">
         <div className="flex flex-col justify-center">
             <div className="bg-gray-200 rounded-lg w-80 text-center p-2 px-4">
                 <Heading label="Sign Up" />
                 <SubHeading label="Enter the following details to create your account" />
-                <InputBox onChange={() => {
-
+                <InputBox onChange={(e) => {
+                    setUserName(e.target.value);
                 }} label="Username" placeholder="John"/>
-                <InputBox onChange={() => {
-                    
+                <InputBox onChange={(e) => {
+                    setPassword(e.target.value);
                 }} label="Password" placeholder="*******"/>
-                <Button onClick={() => {
-
+                <Button onClick={async() => {
+                    const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+                        userName,
+                        password
+                    });
+                    alert("You are signed up");
+                    localStorage.setItem("token", response.data.token);
+                    navigate("/dashboard");
                 }} label="Sign Up" />
                 <ButtonWarning label="Already have an account? " buttonText="SignIn" to={"/signin"} />
             </div>
